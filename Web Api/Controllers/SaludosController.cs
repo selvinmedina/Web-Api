1409cd9bd9a0f1b10b255b1325715cffa3e6fc7a
Selvin Medina <selvinmedina0016@gmail.com>
2021-04-10 18:39:59 -0600

    Agregar archivos de proyecto.

---
+++ Web Api/Controllers/SaludosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Web_Api.Controllers
+{
+    [Route("saludos")]
+    [ApiController]
+    public class SaludosController : ControllerBase
+    {
+        [HttpGet("{nombre}")]
+        public ActionResult<string> ObtenerNombre(string nombre)
+        {
+            return $"Hola {nombre}";
+        }
+
+        [HttpGet("delay/{nombre}")]
+        public async Task<string> ObtenerSaludoConDelay(string nombre)
+        {
+            Console.WriteLine($"Hilo antes del await: {Thread.CurrentThread.ManagedThreadId}");
+
+            await Task.Delay(500);
+            Console.WriteLine($"Hilo despues del await: {Thread.CurrentThread.ManagedThreadId}");
+
+            return $"Hola {nombre}";
+        }
+    }
+}
---
+++ Web Api/Controllers/TarjetasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Web_Api.Helpers;
+
+namespace Web_Api.Controllers
+{
+    [Route("tarjetas")]
+    [ApiController]
+    public class TarjetasController : ControllerBase
+    {
+        public static int cantidadTarjetas = 0;
+        [HttpPost("")]
+        public async Task<ActionResult> ProcesarTarjeta([FromBody] string tarjeta)
+        {
+            var valorAleatorio = RandomGen.NextDouble();
+            var aprobada = valorAleatorio > 0.1;
+
+            Console.WriteLine($"Tarjeta {tarjeta} procesada");
+            Console.WriteLine(++cantidadTarjetas);
+
+            return Ok(new { Tarjeta = tarjeta, Aprobada = aprobada });
+        }
+    }
+}
